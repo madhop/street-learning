cf683af2550d0eaef1849f70f4c7045bad0c8dd3
Unknown <umbertofazio92@gmail.com>
2018-04-29 10:46:08 +0200

    starting a new street_learning

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -2,12 +2,26 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
-a = tf.constant(2, name='a')
+constant_dim = 1
+a = tf.constant([2]*constant_dim, name='a')
 b = tf.constant(3, name='b')
 x = tf.add(a,b, name='add')
 
 writer = tf.summary.FileWriter('./graph', tf.get_default_graph())
-with tf.Session() as sess:
-    # writer = tf.summary.FileWriter('./graphs', sess.graph)
-    print('x:',sess.run(x))
+l = []
+for i in range(0,constant_dim):
+    with tf.Session() as sess:
+        # writer = tf.summary.FileWriter('./graphs', sess.graph)
+        var = sess.run(x)
+        print('x:',sess.run(x))
+l.append(var)
 writer.close()
+
+
+print('l', l)
+
+c = tf.constant(100, name='c')
+add_op = tf.add(l, c)
+
+with tf.Session() as sess:
+    print('add_op:',sess.run(add_op))
